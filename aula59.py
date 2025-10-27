7a8b74ab63c010c869b5af9440cc3f2f0181fd67
emilyfranca8 <emily.franca.ribeiro@escola.pr.gov.br>
2025-10-27 08:03:09 -0300

    aula do dia 27-10

---
+++ aula59.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Arrays de exemplo
+array1 = np.array([10, 20, 30])
+array2 = np.array([5, 15, 25])
+
+# Diferença entre arrays
+diferenca = array1 - array2
+print("Diferença:", diferença)
+
+# Multiplicação entre arrays
+multiplicação = array1 * array2
+print("Multiplicação:", multtiplicação)
